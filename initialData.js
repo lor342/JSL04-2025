c92171aa1c01d92cdb21954bdf8a8a63a711f4d2
lor342 <lor@krs.co.za>
2025-09-14 13:37:41 +0200

    Displaying tiles and modal with data

--- initialData.js
+++ initialData.js
@@ -1,3 +1,5 @@
+// const { createElement } = require("react");
+
 const initialTasks = [
   {
     id: 1,
@@ -21,15 +23,13 @@ const initialTasks = [
   {
     id: 11,
     title: "Learn Data Structures and Algorithms 📚",
-    description:
-      "Study fundamental data structures and algorithms to solve coding problems efficiently",
+    description: "Study fundamental data structures and algorithms to solve coding problems efficiently",
     status: "todo",
   },
   {
     id: 12,
     title: "Contribute to Open Source Projects 🌐",
-    description:
-      "Gain practical experience and collaborate with others in the software development community",
+    description: "Gain practical experience and collaborate with others in the software development community",
     status: "done",
   },
   {
@@ -40,3 +40,36 @@ const initialTasks = [
     status: "done",
   },
 ];
+
+// Extract the todo tasks
+const todoTasks = initialTasks.filter(t => t.status === "todo");
+// <div class="task">Launch Epic Career 🚀</div>
+todoTasks.forEach(element => {
+  const div = document.createElement("div");
+  div.className = "task";
+  div.textContent = element.title;
+  // add on click event to div to invoke the displayModal function
+  div.onclick = () => displayModal(element.id);
+  // append the div to the tasks-todo container in index.html
+  document.querySelector(".tasks-todo").appendChild(div);
+});
+
+
+function displayModal(taskId) {
+  // find taskId in initialTasks
+  let task = initialTasks.find(t => t.id === taskId);  
+  console.log(task);
+
+  // populate the dialog with task details
+  const dialog = document.getElementById("taskDialog");
+  let titleInput = dialog.querySelector('input[name="task-title"]');
+  titleInput.value = task.title;
+  let descInput = dialog.querySelector('textarea[name="task-description"]');
+  descInput.value = task.description;
+  let statusSelect = dialog.querySelector('select[name="task-status"]');
+  statusSelect.value = task.status;
+  // dialog.querySelector("task-description").innerHTML = task.description;
+  // dialog.querySelector("task-status").innerHTML = task.status;
+
+  dialog.showModal();
+}
